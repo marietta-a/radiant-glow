7aae43d1071b7e406e1ae02916e3a38ead823e42
Akumbom <akumbom5ma@gmail.com>
2024-10-03 08:55:59 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/radiantglow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.radiantglow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/user_profile.dart
@@ -0,0 +1,10 @@
+class UserProfile{
+   late String fName;
+   late String lName;
+   late String uName;
+   late String name;
+   late DateTime dob;
+   late String goals;
+   late double height;
+   late double weight;
+}
